bf7154caeacb24b8f3cde43dbc1650c8e98960b5
Addison Schuhardt <addison@schuhardt.net>
2022-08-14 22:57:57 -0700

    fixing native assembly packaging and suppressing some warnings

---
+++ local-repo.bat
@@ -0,0 +1,4 @@
+nuget add .\ProcyonSharp\bin\Release\ProcyonSharp.1.2.1.nupkg -source E:\LocalNugetRepository\
+nuget add .\ProcyonSharp.Bindings\bin\Release\ProcyonSharp.Bindings.1.2.1.nupkg -source E:\LocalNugetRepository\
+nuget add .\ProcyonSharp.Input\bin\Release\ProcyonSharp.Input.1.2.1.nupkg -source E:\LocalNugetRepository\
+nuget add .\ProcyonSharp.Attributes\bin\Release\ProcyonSharp.Attributes.1.2.1.nupkg -source E:\LocalNugetRepository\
